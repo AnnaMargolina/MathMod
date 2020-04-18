3f9753db38f848ecc096d8838948c5f006c2906d
AnnaMargolina <annamargo@mail.ru>
2020-04-18 21:56:29 +0300

    1st exercise

---
+++ intr_1.R
@@ -0,0 +1,12 @@
+rm
+iris
+dim(iris)
+names(iris)
+SL = mean(iris$Sepal.Length) ; SL
+SW = mean(iris$Sepal.Width) ; SW
+PL = mean(iris$Petal.Length) ; PL
+PW = mean(iris$Petal.Width) ; PW
+help("list")
+
+my_list = list(SL, SW, PL, PW)
+my_list
